f191e39bff48d41e1e585d1855ca63bbe84f6507
Idan Saban <idan23s.g@gmail.com>
2019-01-26 18:58:36 +0200

    add loading conponent

---
+++ src/components/Loading/loading.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import '../../style/loading.css'
+
+class Loading extends Component {
+    render() {
+        return (
+            <div id="loading-container">
+                {/* <div class="sk-folding-cube">
+                    <div class="sk-cube1 sk-cube"></div>
+                    <div class="sk-cube2 sk-cube"></div>
+                    <div class="sk-cube4 sk-cube"></div>
+                    <div class="sk-cube3 sk-cube"></div>
+                </div> */}
+                <div className="sk-circle">
+                    <div className="sk-circle1 sk-child"></div>
+                    <div className="sk-circle2 sk-child"></div>
+                    <div className="sk-circle3 sk-child"></div>
+                    <div className="sk-circle4 sk-child"></div>
+                    <div className="sk-circle5 sk-child"></div>
+                    <div className="sk-circle6 sk-child"></div>
+                    <div className="sk-circle7 sk-child"></div>
+                    <div className="sk-circle8 sk-child"></div>
+                    <div className="sk-circle9 sk-child"></div>
+                    <div className="sk-circle10 sk-child"></div>
+                    <div className="sk-circle11 sk-child"></div>
+                    <div className="sk-circle12 sk-child"></div>
+                </div>
+                <h3>LOADING...</h3>
+            </div>
+        )
+    }
+}
+
+export default Loading
