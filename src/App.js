c3acc55d979c3260345f3ef0b59287f29b823f0c
Idan Saban <idan23s.g@gmail.com>
2019-01-27 17:34:05 +0200

    fix style

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import './style/App.css';
 import NavBar from './components/navbar/navbar';
 import Clients from './components/clients/clients';
