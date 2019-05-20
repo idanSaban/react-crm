90fb9dbb02b8b4e4378940297b12f75e0bea59c5
Idan Saban <idan23s.g@gmail.com>
2019-05-20 18:45:29 +0300

    lazy loading components

--- src/App.js
+++ src/App.js
@@ -1,30 +1,45 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import React, { Component, Suspense, lazy } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './style/App.css';
 import NavBar from './components/navbar/navbar';
-import Clients from './components/clients/clients';
-import Actions from './components/actions/actions';
-import Analytics from './components/analytics/analytics';
-import Home from './components/home/home';
-
+import Loading from './components/Loading/loading';
+// import Clients from './components/clients/clients';
+// import Actions from './components/actions/actions';
+// import Analytics from './components/analytics/analytics';
+// import Home from './components/home/home';
 
 class App extends Component {
-
-  render() {
-    return (
-      <Router>
-        <div className="App">
-          <NavBar />
-          <Route path="/" exact render={() => <Home />} />
-          <Route path="/clients" exact render={() => <Clients />} />
-          <Route path="/actions" exact render={() => <Actions />} />
-          <Route path="/analytics" exact render={() => <Analytics />} />
-
-        </div>
-      </Router >
-
-    );
-  }
+	render() {
+		return (
+			<Router>
+				<div className='App'>
+					<NavBar />
+					<Suspense fallback={() => <Loading />}>
+						<Route
+							path='/'
+							exact
+							component={lazy(() => import('./components/home/home'))}
+						/>
+						<Route
+							path='/clients'
+							exact
+							component={lazy(() => import('./components/clients/clients'))}
+						/>
+						<Route
+							path='/actions'
+							exact
+							component={lazy(() => import('./components/actions/actions'))}
+						/>
+						<Route
+							path='/analytics'
+							exact
+							component={lazy(() => import('./components/analytics/analytics'))}
+						/>
+					</Suspense>
+				</div>
+			</Router>
+		);
+	}
 }
 
 export default App;
